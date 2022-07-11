bf63605b143fd529981b228d4d83126646a806ba
tushar-jaiswal-professional <tusharjaiswal.professional@gmail.com>
2022-07-11 15:36:33 +0530

    Completed

---
+++ app.js
@@ -0,0 +1,54 @@
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
+
+function myFunction1() {
+    var popup = document.getElementById("myPopup1");
+    popup.classList.toggle("show1");
+}
+
+function myFunction2() {
+    var popup = document.getElementById("myPopup2");
+    popup.classList.toggle("show2");
+}
+
+function myFunction3() {
+    var popup = document.getElementById("myPopup3");
+    popup.classList.toggle("show3");
+}
+
+function myFunction4() {
+    var popup = document.getElementById("myPopup4");
+    popup.classList.toggle("show4");
+}
+
+function myFunction5() {
+    var popup = document.getElementById("myPopup5");
+    popup.classList.toggle("show5");
+}
+
+function myFunction6() {
+    var popup = document.getElementById("myPopup6");
+    popup.classList.toggle("show6");
+}
+
+function myFunction7() {
+    var popup = document.getElementById("myPopup7");
+    popup.classList.toggle("show7");
+}
+
+function myFunction8() {
+    var popup = document.getElementById("myPopup8");
+    popup.classList.toggle("show8");
+}
+
+function myFunction9() {
+    var popup = document.getElementById("myPopup9");
+    popup.classList.toggle("show9");
+}
+
+function myFunction10() {
+    var popup = document.getElementById("myPopup10");
+    popup.classList.toggle("show10");
+}
